d5816c5e31ed78dad263151fdf9ae7b1e9ef47e9
Justin Muller <wickedjust@hotmail.com>
2018-09-18 21:32:15 +1000

    adding spinner

--- searchable-select-list/src/components/SelectTextbox/index.js
+++ searchable-select-list/src/components/SelectTextbox/index.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import './style.css';
+import spinner from '../../assets/spinner.gif';
 
 export default class SelectTextbox extends PureComponent {
   render() {
     return (
       <div className="select-textbox-container">
         <input className="select-textbox" placeholder="Search.." type="text" />
+        <img alt="loading" src={spinner} className="select-textbox-spinner" />
       </div>
     )
   }
