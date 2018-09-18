bd75ee34c4bc2a5e1167526895b6ac1ae1d11f43
Justin Muller <wickedjust@hotmail.com>
2018-09-18 20:29:16 +1000

    starting to build out components in the view.

--- searchable-select-list/src/components/NavBar/index.js
+++ searchable-select-list/src/components/NavBar/index.js
@@ -1,21 +1,19 @@
 import React, { PureComponent } from 'react';
 import './style.css';
 import logo from '../../assets/marvel.svg';
+import Hamburger from '../Hamburger';
+import SelectTextbox from '../SelectTextbox';
 
 export default class NavBar extends PureComponent {
   render() {
     return (
       <header className="navbar">
-        <button className="hamburger">
-          <span className="hamburger-line" />
-          <span className="hamburger-line" />
-          <span className="hamburger-line" />
-        </button>
+        <Hamburger />
         <div >
           <img src={logo} alt="Marvel" className="logo" />
         </div>
         <div>
-          <input />
+          <SelectTextbox />
         </div>
       </header>
     )
---
+++ searchable-select-list/src/components/SelectTextbox/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import './style.css';
+
+export default class SelectTextbox extends PureComponent {
+  render() {
+    return (
+      <input className="select-textbox" type="text" />
+    )
+  }
+}
