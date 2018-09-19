cb29524e77bc403b35475f34b15a872a4f2bcbb3
Justin Muller <wickedjust@hotmail.com>
2018-09-19 23:00:52 +1000

    updating image not found.

--- searchable-select-list/src/components/SelectRow/index.js
+++ searchable-select-list/src/components/SelectRow/index.js
@@ -14,7 +14,7 @@ class SelectRow extends PureComponent {
     const { description, isActive, name } = this.props;
     return (
       <div className={`select-row ${isActive ? 'active' : ''}`} onClick={this.onClick}>
-        <div className="select-row-profile-thumbnail"></div>
+        <div className="select-row-profile-thumbnail">IMAGE NOT FOUND</div>
         <div className="select-row-profile-bio">
           <h3 className="select-row-profile-bio-name">{name}</h3>
           <p className="select-row-profile-bio-description">{description}</p>
