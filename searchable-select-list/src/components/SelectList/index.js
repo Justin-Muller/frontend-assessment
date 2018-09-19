d590e77a92047d5defa4f7d1627c7b6a1b28bed3
Justin Muller <wickedjust@hotmail.com>
2018-09-19 20:23:33 +1000

    empty state

--- searchable-select-list/src/components/SelectList/index.js
+++ searchable-select-list/src/components/SelectList/index.js
@@ -3,9 +3,21 @@ import './style.css';
 
 export default class SelectList extends PureComponent {
   render() {
+    const isEmpty = true;
+    const searchTerm = 'search term';
+
     return (
       <div className="select-list-container">
-        <div className="select-list">Select list</div>
+        <div className="select-list">
+          {isEmpty ? (
+            <div className="select-list-empty-container">
+              <h2 className="select-list-empty-title">Oh No!</h2>
+              <p className="select-list-empty-body">No results found for "{searchTerm}"</p>
+            </div>
+          ) : (
+            <div>Row</div>
+          )}
+        </div>
       </div>
     )
   }
